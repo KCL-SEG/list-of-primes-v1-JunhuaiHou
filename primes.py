4b90a8e437df77b1a6de7c395eb0ad46b3615fa2
Junhuai Hou <junhuai.hou@kcl.ac.uk>
2022-10-01 20:55:32 +0100

    fix

--- primes.py
+++ primes.py
@@ -1,14 +1,16 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import re
+
 def primes(number_of_primes):
     list = []
+    
+    i = 2
 
-    for number in range(1, number_of_primes):
-        for index in range(2, number):
-            if(number % index == 0):
-                break
-        else:
-            list.append(number)
+    while len(list) < number_of_primes:
+        if(re.match(r'^1?$|^(11+?)\1+$', '1' * i) == None):
+            list.append(i)
+            i +=1
 
     return list
