53870b20eb4f8ad16d264d64c9c8b7bc9df002e7
Junhuai Hou <junhuai.hou@kcl.ac.uk>
2022-10-01 20:40:35 +0100

    finish

--- primes.py
+++ primes.py
@@ -3,4 +3,12 @@
 
 def primes(number_of_primes):
     list = []
+
+    for number in range(1, number_of_primes):
+        for index in range(2, number):
+            if(number % index == 0):
+                break
+        else:
+            list.append(n);
+
     return list
